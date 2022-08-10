96e52156eaffe305baed67d29eedfa63e14a1b98
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-10 17:12:20 +0530

    Minor changes

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Employee {
     String employeeName;
     int employeeCode;
-    double salary=45000.0;
+    double salary;
     String dob;
     int age;
 
@@ -14,14 +14,24 @@ public class Employee {
         return salary+(salary*hikePercentage/100);
     }
 
+    Employee(String employeeName, int employeeCode, double salary, String dob,int age)
+    {
+         this.employeeName= employeeName;
+         this.employeeCode=employeeCode;
+         this.salary=salary;
+         this.dob= dob;
+         this.age=age;
+    }
+
     public static void main(String[] args) {
-        Employee obj = new Employee();
+        Employee obj = new Employee("Shubham",12999,60000.0,"17/11/1897",22);
         Scanner ob = new Scanner(System.in);
         int hikePercentage;
         System.out.println(" Enter the hike Percentage : ");
         hikePercentage= ob.nextInt();
         double res = obj.calculateSalaryHike(hikePercentage);
         System.out.println(" Salary after hike is : "+ res);
+        System.out.println(obj.toString());
     }
 
     @Override
